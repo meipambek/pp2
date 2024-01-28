9233735b58d04e4299ae7d39607fb85f043808d1
meipambek <157216679+meipambek@users.noreply.github.com>
2024-01-28 19:08:47 +0600

    tuples.py

--- lab2/tuples.py
+++ lab2/tuples.py
@@ -13,3 +13,165 @@ print(fruits[-1])
 #4
 fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(fruits[2:5])
+
+#example
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+#example Tuples allow duplicate values:
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+#example Print the number of items in the tuple:
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+#exmaple One item tuple, remember the comma:
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+#example String, int and boolean data types:
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+#example A tuple with strings, integers and boolean values:
+tuple1 = ("abc", 34, True, 40, "male")
+
+#example What is the data type of a tuple?
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+#example Using the tuple() method to make a tuple:
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+#example 
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+#example Print the last item of the tuple:
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+#example 
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+#example This example returns the items from the beginning to, but NOT included, "kiwi":
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+#example This example returns the items from "cherry" and to the end:
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+#example This example returns the items from index -4 (included) to index -1 (excluded)
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+#example Check if "apple" is present in the tuple:
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple"
+        
+#example Convert the tuple into a list to be able to change it:
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+#example Convert the tuple into a list, add "orange", and convert it back into a tuple:
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+#example Create a new tuple with the value "orange", and add that tuple:
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+#example Convert the tuple into a list, remove "apple", and convert it back into a tuple
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+#example The del keyword can delete the tuple completely:thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
+
+#example 
+fruits = ("apple", "banana", "cherry")
+
+#exmaple Unpacking a tuple:
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+#example Assign the rest of the values as a list called "red":
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+#example Add a list of values the "tropic" variable:
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
+
+#example Iterate through the items and print the values:
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+#example Print all items by referring to their index number:
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+
+#example Print all items, using a while loop to go through all the index numbers:
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
+  
+#example Join two tuples:
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+#example Multiply the fruits tuple by 2:
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+
+#example 
+'''
+count()	Returns the number of times a specified value occurs in a tuple
+index()	Searches the tuple for a specified value and returns the position of where it was found
+'''
