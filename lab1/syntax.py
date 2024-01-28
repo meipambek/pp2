378663b4581bf62d8ddf0e5ee6fa1fccc58b71c6
meipambek <157216679+meipambek@users.noreply.github.com>
2024-01-28 17:52:33 +0600

    syntax.py

--- lab1/syntax.py
+++ lab1/syntax.py
@@ -5,3 +5,17 @@ print("Hello World")
 if 5 > 2:
     print("YES")
     
+#Example
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+        
+#Example Syntax Error:
+ if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!") 
+        
+#Example Syntax Error:  
+if 5 > 2:
+print("Five is greater than two!")
